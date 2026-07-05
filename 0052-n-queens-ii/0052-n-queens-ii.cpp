59cdf3777c53998c366c09bd16fe2247371ed270
Yash Kapoor <yash.kpr2708@gmail.com>
2026-07-05 16:30:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (66.68%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> col;
+    vector<int> row;
+    vector<int> posDiag;
+    vector<int> negDiag;
+    int ans = 0;
+
+    bool isSafe(int r, int c, int n) {
+        if(row[r] || col[c] || posDiag[r + c] || negDiag[r - c + n - 1]) return false;
+        return true;
+    }
+
+    void Solve(int r, int n) {
+        if(r == n) {
+            ans++;
+            return;
+        }
+        for(int i = 0; i < n; i++) {
+            if(isSafe(r, i, n)) {
+                row[r] = 1;
+                col[i] = 1;
+                posDiag[r + i] = 1;
+                negDiag[r - i + n - 1] = 1;
+                Solve(r + 1, n);
+                row[r] = 0;
+                col[i] = 0;
+                posDiag[r + i] = 0;
+                negDiag[r - i + n - 1] = 0;
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        col.resize(n, 0);
+        row.resize(n, 0);
+        posDiag.resize(2 * n - 1, 0);
+        negDiag.resize(2 * n - 1, 0);
+        Solve(0, n);
+        return ans;
+    }
+};
