498c993348e12d1bd1d2b9697ffea32562dbf271
Yash Kapoor <yash.kpr2708@gmail.com>
2026-07-27 17:38:56 +0530

    Time: 601 ms (5.37%), Space: 13.9 MB (82.49%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = nums[0] + nums[1] + nums[2];
+        int minimum = abs(target - ans);
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
+                for(int k = j + 1; k < n; k++) {
+                    if(abs(nums[i] + nums[j] + nums[k] - target) < minimum) {
+                        minimum = abs(nums[i] + nums[j] + nums[k] - target);
+                        ans = nums[i] + nums[j] + nums[k];
+                    }
+                }
+            }
+        }
+        return  ans;
+    }
+};
